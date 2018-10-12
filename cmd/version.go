b182e83921cd156497b4e7061c850939424b9f1e
rwesolowski <rwesolowski@greywizard.com>
2018-10-12 09:34:47 +0200

    coinpaprika.com instead of placeholder

--- cmd/version.go
+++ cmd/version.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE coinpaprika.com
+// Copyright © 2018 coinpaprika.com
 //
 // Licensed under the Apache License, version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
